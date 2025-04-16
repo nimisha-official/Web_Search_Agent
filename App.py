8af73f969c0332a7e376f955ee8f83b036b7bc2f
Nimisha Mavar <nimishamavar.official@gmail.com>
2025-04-16 12:38:41 +0530

    Setup groq mixtral llm

--- App.py
+++ App.py
@@ -5,3 +5,11 @@ from langchain_groq import ChatGroq
 from googlesearch import search
 from bs4 import BeautifulSoup
 import requests
+import os
+from dotenv import load_dotenv
+
+# Load secret values from .env
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(api_key=groq_api_key, model_name="mixtral-8x7b-32768")
